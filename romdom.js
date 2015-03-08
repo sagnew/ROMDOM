fa4145b2a28a51d20c7e48e98222b02e43742177
Manuel Lopez <manny.lopez625@gmail.com>
2015-03-08 05:43:20 -0400

    finished writeByte function

--- romdom.js
+++ romdom.js
@@ -1,10 +1,21 @@
-var request = require('request'),
+var fs = require('fs'),
     romdom = {};
 
 romdom.readRamByte = function() {
 };
 
-romdom.writeRamByte = function() {
+romdom.writeRamByte = function(address, value) {
+  fs.writeFile('address.txt', address, function(err){
+    if(err) throw err;
+  });
+
+  fs.writeFile('value.txt', address, function(err){
+    if(err) throw err;
+  });
+
+  fs.writeFile('action.txt', 'writeRamByte', function(err){
+    if(err) throw err;
+  });
 };
 
 module.exports = romdom;
